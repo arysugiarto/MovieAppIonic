9cc9a626264fe9c8860ef3c189c67b9fc879f83c
Ary Sugiarto <arysugiarto10@gmail.com>
2020-06-15 08:44:56 +0700

    update ui details movie

--- src/app/pages/movie-details/movie-details.page.ts
+++ src/app/pages/movie-details/movie-details.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MovieService} from './../../services/movie.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-movie-details',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieDetailsPage implements OnInit {
 
-  constructor() { }
-
+  information = null;
+  
+  constructor(private activatedRoute: ActivatedRoute, private movieService: MovieService) { }
+ 
   ngOnInit() {
+    // Get the ID that was passed with the URL
+    let id = this.activatedRoute.snapshot.paramMap.get('id');
+ 
+    // Get the information from the API
+    this.movieService.getDetails(id).subscribe(result => {
+      this.information = result;
+    });
   }
-
+ 
+  openWebsite() {
+    window.open(this.information.Website, '_blank');
+  }
 }
